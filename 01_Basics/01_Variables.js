d707b7978a42d927de9f85e1ab4f6d314c6ec461
Suraj Singh <108177811+surajsingh2323@users.noreply.github.com>
2024-09-05 08:06:38 +0000

    container config done

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,9 @@
+const accountId = 12345
+let emailID = "suraj@google.com"
+var acccountPassword = "65421"
+acccountCity = "hisar"
+
+// accountId = 2 // not allowed value changing of const keyword
+
+console.log(accountId);
+
