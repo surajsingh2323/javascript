0741e97f6728128994da8d5979461a6738d6441b
Suraj Singh <108177811+surajsingh2323@users.noreply.github.com>
2024-09-09 07:45:00 +0000

    Datatypes

--- 01_Basics/01_Variables.js
+++ 01_Basics/01_Variables.js
@@ -4,6 +4,13 @@ var acccountPassword = "65421"
 acccountCity = "hisar"
 
 // accountId = 2 // not allowed value changing of const keyword
-
+emailID = "surj@gmail.com"
+acccountPassword = "4321"
+acccountCity = "Hansi"
+let accountState;
+console.table([emailID,acccountPassword,acccountCity,accountState])
+//.table used to print multiple values in once
 console.log(accountId);
+/* prefer not to use var because of issue in 
+bloack scope and functional scope */
 
---
+++ 01_Basics/02_Datatypes.js
@@ -0,0 +1,25 @@
+"use strict";//treat all code as newer version
+//alert(3+3)// we are using nodejs not browser
+console.log("Suraj");
+let name = "Suraj"
+let age = 21
+let isLoggedin = false
+let state = null
+
+// number=> 2^53
+//bigint==> used for big numbers like in trade,stock ,redhitweb etc
+//string=> ""
+//boolean=> false/true
+//null=> standalone value
+//undefined=>
+//symbol=> mostly used in react,figma it used to find uniqueness    // these all are primitive data types
+
+
+//object
+
+console.log(typeof "Suraj"); // Outputs: string
+console.log(typeof undefined);// undefined
+console.log(typeof null);//object
+
+
+
