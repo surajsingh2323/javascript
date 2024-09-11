0057d525848801bbb1f74281e1b576d070d8863f
Suraj Singh <108177811+surajsingh2323@users.noreply.github.com>
2024-09-11 06:56:58 +0000

    conversionOperation Done

--- 01_Basics/03_ConversionOperation.js
+++ 01_Basics/03_ConversionOperation.js
@@ -1,4 +1,4 @@
-                              //We are doing investigation of data conversion here :)
+                                                   //We are doing investigation of data conversion here :)
 let score = "45abc"
 
 //console.log(typeof score);
@@ -15,7 +15,48 @@ let booleanisLoggedin = Boolean(isLoggedin)
 //True=>'1' ; False=>'0' and vice versa '0'=>false ; '1'=>true
 let someNumber = 33
 let StringNumber = String(someNumber)
-console.log(StringNumber);
-console.log(typeof StringNumber);
+//console.log(StringNumber);
+//console.log(typeof StringNumber);
 //its gives value as number 33 but type is changed to String
+
+/*********************************OPERATIONS*********************************************************************/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+/*console.log(2+2);
+console.log(2-2);
+console.log(2*2); //2 raise to power 2
+console.log(2**3); //2 raise to the power 3
+console.log(2/3);
+console.log(2%2); % operator used in cryptography and many algorithms */
+let str1 = "Hello"
+let str2 = " coders"
+let str3 = str1+str2
+//console.log(str3);
+//console.log("1"+2);//result = 12
+//console.log(1+"2");//result = 12
+//console.log("1"+2+2);//result = 122
+//console.log(1+2+"2");//result = 32
+// read ecma for type conversion if first is string then all treated as a string; if last is string then first operation done after this string added on that operation
+//console.log((3 + 4) * 5 % 3);//recommended to write operation in() that you want to execute first this is clean code way
+
+/*console.log(+true);// output = 1
+'+' will do type conversion of values in binary but this type of conversion not recommended for clean code 
+console.log(+"");//output = 0 */
+let num1 , num2 , num3
+num1 = num2 = num3 = 3+3
+
+let Gamecounter = 100
+Gamecounter++;//++Gamecounter is prefix which will first increment then store value postfix is vice versa,source read 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Increment' 
+console.log(Gamecounter);
+//To study type conversion
+//follow https://tc39.es/ecma262/#sec-type-conversion
+
+
+
+
+
+
 
