cc9eda5071541fb3250087cda42a4bc7443d05e7
Suraj Singh <108177811+surajsingh2323@users.noreply.github.com>
2024-09-16 11:55:54 +0000

    String Methods discussed

--- 01_Basics/06_Strings.js
+++ 01_Basics/06_Strings.js
@@ -4,10 +4,14 @@ const age = 21
 console.log(`Hello my name is ${name} and my age is ${age}`);
 // this is a string interpolation in which we make place holders ${} where we can inject variables good for using in backend devlopment ,its a same like string template in kotlin 
 const gameName = new String(`Surajhc`);// declaring string another method 
-
+                            // Methods for Strings:-
 console.log(gameName[0]);
 console.log(gameName.__proto__);// for accessing prototype there are amny syntax we used .__prot__ here ;
 // we will get a object {} that seems to empty but there are many values in it, best way to check these is through console(by inspect)
 console.log(gameName.length);
-console.log(gameName.toUpperCase());// use ()at end otherwise it only tell--> function toUppercase and also it using stack memory
+console.log(gameName.toUpperCase());// use ()at end otherwise it only tell--> function toUppercase and also using stack memory
+console.log(gameName.charAt(2));
+console.log(gameName.indexOf('k'));// if give random character that is not in string it gives (-1), unexcpected results
+
+
 
